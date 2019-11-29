e0adadc7d7e2990699dbf4db7e15b75e8eaac1b5
karol.sienkiewicz <karol.sienkiewicz@student.put.poznan.pl>
2019-11-29 11:46:45 +0100

    main mechanik tested and repaired

--- OS/Sync_Mech.cpp
+++ OS/Sync_Mech.cpp
@@ -1,18 +1,18 @@
 //Karol
-#include"Sync_Mech.hpp"
+#include"Sync.h"
 
-void signal_sem() {
+void Semaphore::signal_sem() {
 	value++;
 	PCB* pcb;
 	if (queue.size() != 0) {
 		pcb = queue.at(0);
-		queue.erase(front());
+		queue.erase(queue.begin());
 		pcb->resumeProcess();
 	}
 }
 
-void wait_sem(PCB* pcb) {
-	if (queue <= 0) {
+void Semaphore::wait_sem(PCB* pcb) {
+	if (value <= 0) {
 		value--;
 		queue.push_back(pcb);
 		pcb->haltProcess();
