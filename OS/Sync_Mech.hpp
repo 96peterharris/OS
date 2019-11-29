909dabc59bd21eb9633da8b66bfc4503bc3e98de
karol.sienkiewicz <karol.sienkiewicz@student.put.poznan.pl>
2019-11-29 11:04:04 +0100

    ehm

--- OS/Sync_Mech.hpp
+++ OS/Sync_Mech.hpp
@@ -3,30 +3,15 @@
 #include "PCB.hpp"
 //Karol
 class Semaphore {
-public:
-	unsigned int value;
+protected:
+	int value;
 	std::vector<PCB*> queue;
 
-	Semaphore(unsigned int v) {
-		this->value = v;
-	}
-	~Semaphore() {};
+public:
+	Semaphore(unsigned int v) : value(v) {}
+	~Semaphore() {}
+
+	void signal_sem();
+
+	void wait_sem(PCB* pcb);
 };
-
-void signal_sem(Semaphore* sem) {
-	sem->value++;
-	//sem->queue.wyci¹gniêcie PCB z kolejki procesów i wrzucenie go do kolejki procesów aktywnych;
-	sem->queue.pull_front();
-}
-
-void wait_sem(Semaphore* sem,PCB *itself) {
-	//sem->queue.wywalenie PCB z kolejki procesów aktywnych i wrzucenie go tutaj (itself);
-	int place;
-	for (int i = 0; i < queue.size(); i++) {
-		if (t == queue.at(i)) {
-			place = i;
-		}
-	}
-	std::cout << "Place is " << place << std::endl;
-	value--;
-}
