32595d39a0e554bbf9692737109a257e184e9b5f
karol.sienkiewicz <karol.sienkiewicz@student.put.poznan.pl>
2019-11-29 11:11:19 +0100

    now hopefully

--- OS/Sync_Mech.cpp
+++ OS/Sync_Mech.cpp
@@ -1,1 +1,23 @@
 //Karol
+#include"Sync_Mech.hpp"
+
+void signal_sem() {
+	value++;
+	PCB* pcb;
+	if (queue.size() != 0) {
+		pcb = queue.at(0);
+		queue.erase(front());
+		pcb->resumeProcess();
+	}
+}
+
+void wait_sem(PCB* pcb) {
+	if (queue <= 0) {
+		value--;
+		queue.push_back(pcb);
+		pcb->haltProcess();
+	}
+	else {
+		value--;
+	}
+}
