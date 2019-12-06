e80afd12e15a86fbf9e6b8605ffdfbabcb63e47a
VoiTee <wuriakpl2gmail.com>
2019-12-06 15:07:47 +0100

    sranie

--- OS/Interprocess_Com.cpp
+++ OS/Interprocess_Com.cpp
@@ -1,1 +1,26 @@
 //Wojtek
+
+#include "Interprocess_Com.hpp"
+#include "PCB.hpp"
+
+Message::Message() {}
+
+Message::Message(std::string sender, std::string content) {
+	this->pid_sender = sender;
+	this->content = content;
+
+	this->size = content.size();
+
+	//this->size = loadToRam() ???czekam na kinie
+
+}
+
+
+bool PCB::sendMessage(std::string pid_receiver, std::string content) {
+
+	return true;
+}
+bool PCB::receiveMessage(std::string pid_sender) {
+
+	return true;
+}
