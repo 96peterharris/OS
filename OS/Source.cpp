e0129e0cc6099d637e14afac3c39595f4ac0cb8d
seker212 <seker212@gmail.com>
2019-12-15 20:21:50 +0100

    WORKS - arythm. Last commit before general merge

--- OS/Source.cpp
+++ OS/Source.cpp
@@ -2,20 +2,20 @@
 
 int main() {
 	PCB::createDummy();
-	PCB::createProcess("p2","pr2",5);
+	PCB::createProcess("p","si",5);
 	PCB::printMap();
 	PCB::update();
 	PCB::printReadyQueue();
-	System::VM.printVM(PCB::getPCB("p2"));
+	//System::VM.printVM(PCB::getPCB("p2"));
 	std::cout << std::endl;
-	System::RAM.printProcess("p2");
+	//System::RAM.printProcess("p2");
 	for (int i = 0; i < 50; i++)
 	{
 		System::CPU.nexStep();
 		std::cout << "==============================" << std::endl << "Running: ";
 		System::CPU.displayRunning();
-		std::cout << "Procesess: " << std::endl;
-		PCB::printMap();
+		//std::cout << "Procesess: " << std::endl;
+		//PCB::printMap();
 		//System::RAM.printProcess(PCB::getPCB("p1"));
 		//System::RAM.printAllRam();
 
