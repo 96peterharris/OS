2fada0bbda7df9aa5534d5660387a646b965bedc
seker212 <seker212@gmail.com>
2019-12-16 16:45:34 +0100

    bbbbbb

--- OS/Source.cpp
+++ OS/Source.cpp
@@ -6,21 +6,24 @@ int main() {
 	//PCB::printMap();
 	//PCB::update();
 	std::cout << std::endl;
-	/*for (int i = 0; i < 50; i++)
+	for (int i = 0; i < 50; i++)
 	{
 		System::CPU.nextStep();
 		System::CPU.displayRunning();
 		std::cout << "Command: " << i << std::endl << "Procesess: " << std::endl;
 		PCB::printMap();
-		if (i > 9) {
+		if (i > 6) {
 			//System::FS.displaydrivecontent();
 			//System::FS.displaycatalogue();
 			System::RAM.printAllRam();
+			System::VM.printPCBsegTab("p1");
+			System::VM.printPCBsegTab("f1");
+			System::VM.printPCBsegTab("DM");
 		}
 		//System::RAM.printProcess(PCB::getPCB("p1"));
 		//System::RAM.printAllRam();
 		std::cout << std::endl;
-	}*/
+	}
 	
 		/*System::FS.createFile("hi");
 		System::FS.openFile("hi");
